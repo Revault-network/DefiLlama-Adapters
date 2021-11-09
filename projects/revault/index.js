ffb35a4f4d9730b75be9b69db68ef7132945bf9f
Amir Baer <baeramir@gmail.com>
2021-11-09 08:15:02 +0200

    .

--- projects/revault/index.js
+++ projects/revault/index.js
@@ -28,49 +28,6 @@ async function tvl(timestamp, block) {
   return balances;
 }
 
-/*
-
-  const tokenInfo = await revaChefContract.tokens(token.address);
-  const totalTokenPrincipal = tokenInfo.totalPrincipal;
-  const tokenToBusdRate = await zapContract.getBUSDValue(
-    token.address,
-    BigNumber.from("10").pow(tokenDecimals),
-  );
-  const tvlBusd = totalTokenPrincipal
-    .mul(tokenToBusdRate)
-    .div(BigNumber.from("10").pow(tokenDecimals));
-
-}
-
-
-
-const POOL = '0xa1e72267084192db7387c8cc1328fade470e4149';
-const stkTRU = '0x23696914Ca9737466D8553a2d619948f548Ee424';
-const TRU = '0x4C19596f5aAfF459fA38B0f7eD92F11AE6543784';
-const TUSD = '0x0000000000085d4780B73119b644AE5ecd22b376';
-
-
-async function tvl(timestamp, block) {
-    let balances = {};
-
-    const poolTVL = await sdk.api.abi.call({
-      target: POOL,
-      abi: abi['poolValue'],
-      block: block 
-    });
-    const truTVL = await sdk.api.abi.call({
-      target: stkTRU,
-      abi: abi['stakeSupply'],
-      block: block 
-    });
-    
-    balances[TUSD] = poolTVL.output;
-    balances[TRU] = truTVL.output;
-    
-    return balances;
-}
-*/
-
 module.exports = {
   name: 'Revault Network',
   website: 'https://app.revault.network',
